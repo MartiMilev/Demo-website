2fc32db54a0e18e36930f3bc26fec75b3b000598
MartiMilev <MMilev71@students.softuni.bg>
2022-09-14 00:03:25 +0300

    Start Menu
    Login disign

---
+++ Login/Login/wwwroot/js/site.js
@@ -0,0 +1,6 @@
+﻿var button1 = document.getElementById("button1");
+button1.addEventListener("click", function (e) {
+    // e is the event object
+    // e.target is the button element
+    // do stuff with them
+});
